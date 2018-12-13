ae7f925fd11871074bbf142e7b433e38da38395b
高田穂乃花 <kikusui1kaminasizuki0@gmail.com>
2018-12-13 16:13:19 +0900

    Initial Commit

---
+++ GpsStalker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GpsStalker
+//
+//  Created by 高田穂乃花 on 2018/12/13.
+//  Copyright © 2018 stalker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
